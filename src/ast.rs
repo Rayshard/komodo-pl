7bb24ed41b0d11780682baa695298c736111e3a8
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-05-21 10:07:30 -0400

    added node meta

--- src/ast.rs
+++ src/ast.rs
@@ -1,11 +1,34 @@
 use crate::utils::{Position, Span};
 use core::fmt::Debug;
 
-pub trait Node {
-    fn get_span(&self) -> &Span;
-    fn get_node_type(&self) -> NodeType;
-    fn get_ts_type(&self) -> &Option<String>;
+#[derive(Debug)]
+pub struct NodeMeta {
+    span: Span,
+    ts_type: Option<String>,
 }
+
+impl NodeMeta {
+    pub fn new(start: Position, end: Position) -> NodeMeta {
+        NodeMeta {
+            span: Span::new(start, end),
+            ts_type: None,
+        }
+    }
+}
+
+pub trait Node {
+    fn get_node_meta(&self) -> &NodeMeta;
+    fn get_node_type(&self) -> NodeType;
+
+    fn get_span(&self) -> &Span {
+        &self.get_node_meta().span
+    }
+
+    fn get_ts_type(&self) -> &Option<String> {
+        &self.get_node_meta().ts_type
+    }
+}
+
 
 #[derive(Debug)]
 pub enum NodeType<'a> {
@@ -19,7 +42,6 @@ impl Debug for dyn Node {
             "{:?}",
             match self.get_node_type() {
                 NodeType::Expr(expr) => expr,
-                _ => unimplemented!(),
             }
         )
     }
@@ -28,35 +50,26 @@ impl Debug for dyn Node {
 #[derive(Debug)]
 pub enum Expression {
     Binop {
+        meta: NodeMeta,
         left: Box<Expression>,
         op: String,
         right: Box<Expression>,
-        span: Span,
-        ts_type: Option<String>,
     },
     IntLit {
+        meta: NodeMeta,
         value: String,
-        span: Span,
-        ts_type: Option<String>,
     },
 }
 
 impl Node for Expression {
-    fn get_span(&self) -> &Span {
+    fn get_node_meta(&self) -> &NodeMeta {
         match self {
-            Expression::Binop { span, .. } => span,
-            Expression::IntLit { span, .. } => span,
+            Expression::Binop { meta, .. } => meta,
+            Expression::IntLit { meta, .. } => meta,
         }
     }
 
     fn get_node_type(&self) -> NodeType {
         NodeType::Expr(self)
-    }
-
-    fn get_ts_type(&self) -> &Option<String> {
-        match self {
-            Expression::Binop { ts_type, .. } => ts_type,
-            Expression::IntLit { ts_type, .. } => ts_type,
-        }
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use crate::ast::{Expression, Node, NodeType};
+use crate::ast::{Expression, Node, NodeMeta, NodeType};
 use crate::lexer::token::{Token, TokenType};
 use crate::utils::{Position, Span};
 use std::cmp;
@@ -36,24 +36,19 @@ impl<'a> TokenStream<'a> {
 
 pub fn parse(tokens: Vec<Token>) -> Box<dyn Node> {
     let left = Box::new(Expression::IntLit {
+        meta: NodeMeta::new(Position::new(1, 1), Position::new(1, 2)),
         value: String::from("123"),
-        span: Span::new(Position::new(1, 1), Position::new(1, 2)),
-        ts_type: None,
     });
 
     let right = Box::new(Expression::IntLit {
+        meta: NodeMeta::new(Position::new(1, 4), Position::new(1, 5)),
         value: String::from("123"),
-        span: Span::new(Position::new(1, 4), Position::new(1, 5)),
-        ts_type: None,
     });
 
-    let span = Span::new(left.get_span().start, right.get_span().end);
-
     Box::new(Expression::Binop {
+        meta: NodeMeta::new(left.get_span().start, right.get_span().end),
         left: left,
         op: String::from("+"),
         right: right,
-        span: span,
-        ts_type: None,
     })
 }
